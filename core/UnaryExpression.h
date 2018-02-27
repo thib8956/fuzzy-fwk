a6159e96c3cf4ab7d289b272547e8cc35a24d7d1
Thibaud <thibaud.gasser@uha.fr>
2018-02-27 22:23:09 +0100

    Add unary expression base class

--- core/UnaryExpression.h
+++ core/UnaryExpression.h
@@ -1,12 +1,21 @@
 #ifndef UNARYEXPRESSION_H_
 #define UNARYEXPRESSION_H_
 
+#include "Expression.h"
+
 namespace core {
 	template <typename T>
 	class UnaryExpression {
 	public:
 		virtual ~UnaryExpression() = default;
-		virtual T* evaluate(T* expression) const = 0;
+		virtual T evaluate(Expression<T> *expression) const = 0;
+	};
+
+	template <typename T>
+	class Not : public UnaryExpression<T> {
+	public:
+		virtual ~Not() = default;
+		virtual T evaluate(Expression<T> *expression) const = 0;
 	};
 }  // namespace core
 
