2bb7567d018c022060e3439cab6aa85497652712
Thibaud <thibaud.gasser@uha.fr>
2018-02-27 22:23:59 +0100

    Add NotMinus

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 #include "core/ValueModel.h"
+#include "fuzzy/NotMinus1.h"
+#include "core/UnaryExpressionModel.h"
 
 int main(int argc, char* const argv[]) {
 	core::ValueModel<int> value;
-	int v(2);
+	int v(5);
 	value.setValue(&v);
 
 	std::cout << "Value : " << value.evaluate() << std::endl;
+
+    fuzzy::NotMinus1<int> expr;
+    core::UnaryExpressionModel<int> model(&value, &expr);
+	std::cout << "NotMinus1 : " << model.evaluate();
 }
