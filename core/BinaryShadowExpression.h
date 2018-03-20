940020674d1b63bd451545deb283ce7693f7c657
Thibaud <thibaud.gasser@uha.fr>
2018-03-20 09:51:47 +0100

    Add getters and setters in ShadowExpressions

--- core/BinaryShadowExpression.h
+++ core/BinaryShadowExpression.h
@@ -2,17 +2,26 @@
 #define BINARYSHADOWEXPRESSION_H_
 
 #include "BinaryExpression.h"
+#include "Expression.h"
 
 namespace core {
 	template <typename T>
-    class BinaryShadowExpression : public BinaryExpression{
+    class BinaryShadowExpression : public BinaryExpression<T> {
 	public:
+		BinaryShadowExpression(BinaryExpression<T> *target = nullptr);
 		virtual ~BinaryShadowExpression() = default;
-		virtual T evaluate(Expression<T> *left, Expression<T> *right);
+		virtual T evaluate(Expression<T> *left, Expression<T> *right) const;
+	const BinaryExpression<T>* getTarget() const;
+	void setTarget(BinaryExpression<T>* target);
+
     private:
         BinaryExpression<T> *target; // target
 	};
-    
+
+	template<typename T>
+	BinaryShadowExpression<T>::BinaryShadowExpression(BinaryExpression<T>* target) : target(target) {
+	}
+
     template <typename T>
     T BinaryShadowExpression<T>::evaluate(Expression<T> *left, Expression<T> *right) const {
         if (target != nullptr) {
@@ -20,8 +29,16 @@ namespace core {
         }
         return T();
     }
+
+    template <typename T>
+    const BinaryExpression<T>* BinaryShadowExpression<T>::getTarget() const {
+    	return target;
+    }
+
+    template <typename T>
+    void BinaryShadowExpression<T>::setTarget(BinaryExpression<T>* target) {
+    	this->target = target;
+    }
 } // namespace core
-
-
 
 #endif /* BINARYSHADOWEXPRESSION_H_ */
--- core/UnaryShadowExpression.h
+++ core/UnaryShadowExpression.h
@@ -1,18 +1,26 @@
 #ifndef UNARYSHADOWEXPRESSION_H_
 #define UNARYSHADOWEXPRESSION_H_
 
+#include "Expression.h"
 #include "UnaryExpression.h"
 
 namespace core {
 	template <typename T>
     class UnaryShadowExpression : public UnaryExpression<T>{
 	public:
+		UnaryShadowExpression(UnaryExpression<T> *target = nullptr);
 		virtual ~UnaryShadowExpression() = default;
-		virtual T evaluate(Expression<T> *expression);
+		virtual T evaluate(Expression<T> *expression) const;
+		const UnaryExpression<T>* getTarget() const;
+		void setTarget(UnaryExpression<T>* target);
+
     private:
-        UnaryExpression<T> *target; // target
+        UnaryExpression<T> *target;
     };
-    
+
+	template <typename T>
+	UnaryShadowExpression<T>::UnaryShadowExpression(UnaryExpression<T> *target) :	target(target) {
+	}
     
     template <typename T>
     T UnaryShadowExpression<T>::evaluate(Expression<T> *expression) const {
@@ -22,6 +30,16 @@ namespace core {
         return T();
     }
     
+    template <typename T>
+    const UnaryExpression<T>* UnaryShadowExpression<T>::getTarget() const {
+    	return target;
+    }
+
+    template <typename T>
+    void UnaryShadowExpression<T>::setTarget(UnaryExpression<T>* target) {
+    	this->target = target;
+    }
+
 }  // namespace core
 
 #endif /* UNARYSHADOWEXPRESSION_H_ */
