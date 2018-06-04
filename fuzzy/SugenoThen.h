9ca53876b454a09203638f45bdf144b1e2a452b8
Thibaud <thibaud.gasser@uha.fr>
2018-06-04 20:17:27 +0200

    Add Sugeno Then

---
+++ fuzzy/SugenoThen.h
@@ -0,0 +1,40 @@
+#ifndef CORE_SUGENOTHEN_H_
+#define CORE_SUGENOTHEN_H_
+
+#include "../core/Expression.h"
+#include "../core/Then.h"
+
+namespace fuzzy {
+
+template <typename T>
+class SugenoThen : public core::Then<T> {
+public:
+	SugenoThen();
+	virtual ~SugenoThen() = default;
+
+	virtual T evaluate(core::Expression<T> *left, core::Expression<T> *right) const;
+	virtual T getPremiseValue() const;
+
+private:
+	// TODO : mutable ?
+	T premiseValue;
+};
+
+template <typename T>
+SugenoThen<T>::SugenoThen() : premiseValue(0) {}
+
+template <typename T>
+T SugenoThen<T>::evaluate(core::Expression<T>* left, core::Expression<T>* right) const {
+	premiseValue = left->evaluate();
+	T rightEval = right->evaluate();
+	return premiseValue * rightEval;
+}
+
+template <typename T>
+T SugenoThen<T>::getPremiseValue() const {
+		return premiseValue;
+}
+
+}
+
+#endif /* CORE_SUGENOTHEN_H_ */
