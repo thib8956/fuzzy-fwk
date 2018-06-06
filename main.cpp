6f358cfe68e192e6efba0b33c88e229be301db6f
Thibaud <thibaud.gasser@uha.fr>
2018-06-06 11:29:08 +0200

    Add grades mamdani example

--- main.cpp
+++ main.cpp
@@ -143,15 +143,78 @@ void tipsSugeno() {
 	}
 }
 
+void gradesMamdani() {
+	// operators
+	NotMinus1<float> opeNot;
+	AndMin<float> opeAnd;
+	OrMax<float> opeOr;
+	ThenMin<float> opeThen;
+	AggMax<float> opeAgg;
+	CogDefuzz<float> opDefuzz(0, 20, 1);
+
+	FuzzyFactory<float> factory(&opeNot, &opeAnd, &opeOr, &opeThen, &opeAgg, &opDefuzz);
+
+	// Input variables : care and work
+	ValueModel<float> care(0);
+	ValueModel<float> work(0);
+
+	// Output : grade
+	ValueModel<float> grade(0);
+
+	// Membership functions
+	IsTriangle<float> poor(-5, 0, 5);
+	IsTriangle<float> good(0, 5, 10);
+	IsTriangle<float> excellent(10, 20, 30);
+	IsTriangle<float> bad(0, 5, 10);
+	IsTriangle<float> careful(10, 15, 20);
+
+	Expression<float> *expr = factory.newAgg(
+			factory.newAgg(
+					// IF work IS poor OR care is bad THEN grade IS bad
+					factory.newThen(
+							factory.newOr(
+									factory.newIs(&poor, &work),
+									factory.newIs(&bad, &care)
+							),
+							factory.newIs(&bad, &grade)
+					),
+					// IF work IS good THEN grade IS good
+					factory.newThen(
+							factory.newIs(&good, &work),
+							factory.newIs(&good, &grade)
+					)
+			),
+			// IF work IS excellent AND care IS good THEN grade IS excellent
+			factory.newThen(
+					factory.newAnd(
+							factory.newIs(&excellent, &work),
+							factory.newIs(&careful, &care)
+					),
+					factory.newIs(&excellent, &grade)
+			)
+	);
+
+	Expression<float> *system = factory.newDefuzz(&grade, expr);
+
+	std::cout << "Grades test." << std::endl;
+	float s;
+	while (true) {
+		std::cout << "Work : ";
+		std::cin >> s;
+		work.setValue(&s);
+		std::cout << "Care : ";
+		std::cin >> s;
+		care.setValue(&s);
+		try {
+			std::cout << "Grade -> " << system->evaluate() << std::endl;
+		} catch (std::runtime_error& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	}
 int main(void) {
-//	CogDefuzz<float> opDefuzz(0, 5, 1);
-//	IsTriangle<float> cheap(0, 5, 10);
-//
-//	ValueModel<float> output(0);
-//	ValueModel<float> val(2);
-//	UnaryExpressionModel<float> cheapModel(&val, &cheap);
-//	float eval = opDefuzz.evaluate(&output, &cheapModel);
-//	std::cout << eval << std::endl;
 	//tipsMamdani();
-	tipsSugeno();
+	//tipsSugeno();
+	gradesMamdani();
 }
